540c0fe6b2bdc854077c993eaf7055627e2db54f
NoctisBeta <noctisbeta@gmail.com>
2021-10-08 03:19:18 +0200

    arc fix

--- prod_app_3/lib/day_summary.dart
+++ prod_app_3/lib/day_summary.dart
@@ -38,12 +38,9 @@ class _DaySummaryState extends State<DaySummary> {
   }
 
   void buildArcs() {
-    print('buildam arce');
     if (events.isNotEmpty) {
       for (int i = 0; i < events.length; i++) {
-        print('buildam arce 4');
         arcs?.add(Arc(events[i]));
-        print('buildam arce 2');
         arcsDisplay?.add(SizedBox(
           height: 100,
           width: 100,
@@ -81,6 +78,13 @@ class _DaySummaryState extends State<DaySummary> {
                     ),
                   ),
                   ...?arcsDisplay,
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: CustomPaint(
+                      painter: TimePointerPainter(),
+                    ),
+                  ),
                 ]),
               ),
             ),
@@ -161,77 +165,77 @@ class _DaySummaryState extends State<DaySummary> {
   }
 }
 
-class Arcs extends CustomPainter {
-  List<Event> events;
+// class Arcs extends CustomPainter {
+//   List<Event> events;
 
-  Arcs(this.events);
+//   Arcs(this.events);
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    final centerX = size.width / 2;
-    final centerY = size.height / 2;
-    final center = Offset(centerX, centerY);
-    final radius = min(centerX, centerY);
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final centerX = size.width / 2;
+//     final centerY = size.height / 2;
+//     final center = Offset(centerX, centerY);
+//     final radius = min(centerX, centerY);
 
-    // var fillBrush2 = Paint()..color = Color(0xff303030);
-    final fillBrush2 = Paint()..color = Colors.indigoAccent;
-    final Rect rect = Rect.fromCenter(center: center, width: 180, height: 180);
+//     // var fillBrush2 = Paint()..color = Color(0xff303030);
+//     final fillBrush2 = Paint()..color = Colors.indigoAccent;
+//     final Rect rect = Rect.fromCenter(center: center, width: 180, height: 180);
 
-    if (events.isNotEmpty) {
-      for (int i = 0; i < events.length; i++) {
-        double radStart;
-        double radEnd;
-        final temp = events[i].from;
-        final temp2 = events[i].to;
-        radStart = temp.hour * pi / 12 + temp.minute * pi / 720 - pi / 2;
-        radEnd =
-            temp2.hour * pi / 12 + temp2.minute * pi / 720 - pi / 2 - radStart;
-        if (temp2.hour == 0) {
-          radEnd = 2 * pi - radStart - pi / 2;
-        }
-        final fillBrush = Paint()..color = events[i].color!;
-        canvas.drawArc(rect, radStart, radEnd, true, fillBrush);
-      }
-    }
+//     if (events.isNotEmpty) {
+//       for (int i = 0; i < events.length; i++) {
+//         double radStart;
+//         double radEnd;
+//         final temp = events[i].from;
+//         final temp2 = events[i].to;
+//         radStart = temp.hour * pi / 12 + temp.minute * pi / 720 - pi / 2;
+//         radEnd =
+//             temp2.hour * pi / 12 + temp2.minute * pi / 720 - pi / 2 - radStart;
+//         if (temp2.hour == 0) {
+//           radEnd = 2 * pi - radStart - pi / 2;
+//         }
+//         final fillBrush = Paint()..color = events[i].color!;
+//         canvas.drawArc(rect, radStart, radEnd, true, fillBrush);
+//       }
+//     }
 
-    canvas.drawCircle(center, radius, fillBrush2);
-    final fillBrush3 = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2
-      ..color = Colors.black;
-    canvas.drawCircle(center, radius, fillBrush3);
-    canvas.drawCircle(center, size.width - 8.8, fillBrush3);
+//     canvas.drawCircle(center, radius, fillBrush2);
+//     final fillBrush3 = Paint()
+//       ..style = PaintingStyle.stroke
+//       ..strokeWidth = 2
+//       ..color = Colors.black;
+//     canvas.drawCircle(center, radius, fillBrush3);
+//     canvas.drawCircle(center, size.width - 8.8, fillBrush3);
 
-    final fillBrush4 = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2
-      ..color = Colors.black;
+//     final fillBrush4 = Paint()
+//       ..style = PaintingStyle.stroke
+//       ..strokeWidth = 2
+//       ..color = Colors.black;
 
-    final fillBrush5 = Paint()
-      ..style = PaintingStyle.fill
-      ..strokeWidth = 2
-      ..color = Colors.black;
+//     final fillBrush5 = Paint()
+//       ..style = PaintingStyle.fill
+//       ..strokeWidth = 2
+//       ..color = Colors.black;
 
-    final currentTime = DateTime.now();
-    // print(currentTime.hour);
-    final currentTimeAngle =
-        currentTime.hour * pi / 12 + currentTime.minute * pi / 720 - pi / 2;
-    // canvas.translate(centerX, centerY);
-    // canvas.rotate(currentTimeAngle);
-    final hourX = centerX + cos(currentTimeAngle) * radius;
-    final hourY = centerY + sin(currentTimeAngle) * radius;
-    final hourXe = centerX + cos(currentTimeAngle) * 89;
-    final hourYe = centerX + sin(currentTimeAngle) * 89;
+//     final currentTime = DateTime.now();
+//     // print(currentTime.hour);
+//     final currentTimeAngle =
+//         currentTime.hour * pi / 12 + currentTime.minute * pi / 720 - pi / 2;
+//     // canvas.translate(centerX, centerY);
+//     // canvas.rotate(currentTimeAngle);
+//     final hourX = centerX + cos(currentTimeAngle) * radius;
+//     final hourY = centerY + sin(currentTimeAngle) * radius;
+//     final hourXe = centerX + cos(currentTimeAngle) * 89;
+//     final hourYe = centerX + sin(currentTimeAngle) * 89;
 
-    canvas.drawLine(Offset(hourX, hourY), Offset(hourXe, hourYe), fillBrush4);
-    canvas.drawCircle(Offset(hourXe, hourYe), 3, fillBrush5);
+//     canvas.drawLine(Offset(hourX, hourY), Offset(hourXe, hourYe), fillBrush4);
+//     canvas.drawCircle(Offset(hourXe, hourYe), 3, fillBrush5);
 
-    // canvas.drawArc(rect, currentTimeAngle, currentTimeAngle, true, fillBrush4);
-  }
+//     // canvas.drawArc(rect, currentTimeAngle, currentTimeAngle, true, fillBrush4);
+//   }
 
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
-}
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) => true;
+// }
 
 class CircleFramePainter extends CustomPainter {
   @override
@@ -251,6 +255,56 @@ class CircleFramePainter extends CustomPainter {
       ..color = Colors.black;
     canvas.drawCircle(center, radius, fillBrush3);
     canvas.drawCircle(center, size.width - 8.8, fillBrush3);
+
+    // final fillBrush4 = Paint()
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeWidth = 2
+    //   ..color = Colors.black;
+
+    // final fillBrush5 = Paint()
+    //   ..style = PaintingStyle.fill
+    //   ..strokeWidth = 2
+    //   ..color = Colors.black;
+
+    // final currentTime = DateTime.now();
+    // // print(currentTime.hour);
+    // final currentTimeAngle =
+    //     currentTime.hour * pi / 12 + currentTime.minute * pi / 720 - pi / 2;
+    // // canvas.translate(centerX, centerY);
+    // // canvas.rotate(currentTimeAngle);
+    // final hourX = centerX + cos(currentTimeAngle) * radius;
+    // final hourY = centerY + sin(currentTimeAngle) * radius;
+    // final hourXe = centerX + cos(currentTimeAngle) * 89;
+    // final hourYe = centerX + sin(currentTimeAngle) * 89;
+
+    // // canvas.drawLine(Offset(hourX, hourY), Offset(hourXe, hourYe), fillBrush4);
+    // canvas.drawCircle(Offset(hourXe, hourYe), 3, fillBrush5);
+
+    // canvas.drawArc(rect, currentTimeAngle, currentTimeAngle, true, fillBrush4);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class TimePointerPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final centerX = size.width / 2;
+    final centerY = size.height / 2;
+    final center = Offset(centerX, centerY);
+    final radius = min(centerX, centerY);
+
+    // var fillBrush2 = Paint()..color = Color(0xff303030);
+    // final fillBrush2 = Paint()..color = Colors.indigoAccent;
+    // canvas.drawCircle(center, radius, fillBrush2);
+
+    // final fillBrush3 = Paint()
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeWidth = 2
+    //   ..color = Colors.black;
+    // canvas.drawCircle(center, radius, fillBrush3);
+    // canvas.drawCircle(center, size.width - 8.8, fillBrush3);
 
     final fillBrush4 = Paint()
       ..style = PaintingStyle.stroke
@@ -285,8 +339,15 @@ class CircleFramePainter extends CustomPainter {
 
 class ArcPainter extends CustomPainter {
   Event? event;
+  final Path path = Path();
 
   ArcPainter(this.event);
+
+  @override
+  bool hitTest(Offset position) {
+    final bool hit = path.contains(position);
+    return hit;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -313,12 +374,11 @@ class ArcPainter extends CustomPainter {
         ..color = event!.color!
         ..style = PaintingStyle.stroke
         ..strokeWidth = 40;
-      canvas.drawArc(rect, radStart, radEnd, false, fillBrush);
+      // canvas.drawArc(rect, radStart, radEnd, false, fillBrush);
+
+      path.arcTo(rect, radStart, radEnd, true);
+      canvas.drawPath(path, fillBrush);
     }
-    // final rect2 = Rect.largest;
-    // Rect.
-    // final fillBrush = Paint()..color = event!.color!;
-    // canvas.drawRect(rect2, fillBrush);
   }
 
   @override
@@ -326,7 +386,7 @@ class ArcPainter extends CustomPainter {
 }
 
 class Arc extends StatefulWidget {
-  final Event? event;
+  final Event event;
   const Arc(this.event, {Key? key}) : super(key: key);
 
   @override
@@ -336,12 +396,56 @@ class Arc extends StatefulWidget {
 class _ArcState extends State<Arc> {
   @override
   Widget build(BuildContext context) {
-    print('buildam arce 3');
     return GestureDetector(
       onTap: () => {print('tappppp')},
       child: CustomPaint(
         painter: ArcPainter(widget.event),
       ),
     );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return GestureDetector(
+  //     onTap: () => {print('tappppp')},
+  //     child: ClipPath(
+  //       clipper: ArcClipper(widget.event),
+  //     ),
+  //   );
+  // }
+}
+
+class ArcClipper extends CustomClipper<Path> {
+  Event event;
+  Path path = Path();
+
+  ArcClipper(this.event);
+
+  @override
+  Path getClip(Size size) {
+    final centerX = size.width / 2;
+    final centerY = size.height / 2;
+    final center = Offset(centerX, centerY);
+
+    const double rectSize = 141;
+    final Rect rect =
+        Rect.fromCenter(center: center, width: rectSize, height: rectSize);
+
+    double radStart;
+    double radEnd;
+    final temp = event.from;
+    final temp2 = event.to;
+    radStart = temp.hour * pi / 12 + temp.minute * pi / 720 - pi / 2;
+    radEnd = temp2.hour * pi / 12 + temp2.minute * pi / 720 - pi / 2 - radStart;
+    if (temp2.hour == 0) {
+      radEnd = 2 * pi - radStart - pi / 2;
+    }
+    path.arcTo(rect, radStart, radEnd, true);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
